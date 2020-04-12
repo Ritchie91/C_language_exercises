0bd628c1848c47fdd47abfca397a9221b4ca59c9
Ricardo Sanchez <ingricharsan@gmail.com>
2020-04-12 06:44:33 +0000

    Esta nueva version esta comentada

--- contact_list.c
+++ contact_list.c
@@ -1,3 +1,8 @@
+/*************** Manejo dinamico de memoria **********/
+/********************  11/04/2020  *******************/
+/*Este programa ilustra el manejo dinamico de memoria*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,24 +18,38 @@ typedef struct {
 int main( int argc, const char * argv[])
 {
 	CONTACT * list = NULL;
-	char buffer[100];
-	int goOn, listSize = 0;
+
+	char buffer[100]; //Variable auxiliar para guardar las cadenas ingresadas por el usuario   
+	int goOn, listSize = 0; //goOn es la bandera para romper el bucle while
 
 	do {
 		printf("Ingrese el nombre del nuevo contacto (0 para terminar):\n");
 		scanf("%99s", buffer);
-		if(strcmp("0", buffer) != 0){
-			if( !list ) {
+		if(strcmp("0", buffer) != 0) //Si buffer es igual a "0" el if no se ejecuta
+		{
+			if( !list )//El inverso de NULL es uno, por tanto, se ejecuta el if. 
+			{
+				//Se reserva una cantidad de memoria del tamaño de un
+				//CONTACT y ademas inicializa el puntero list:
+				
 				list = malloc( sizeof( CONTACT ) );
-			} else {
+
+			} else // Se ejecuta el else si list ya tiene una direccion de memoria 
+			       // apuntada	
+			{ 
+				//Se reasigna la cantidad de memoria en list, a medida que el 
+				//usuario ingresa mas contactos:
+
 				list = realloc( list, sizeof( CONTACT ) * (listSize + 1 ) );
 			}
-			strcpy( list[ listSize ].name, buffer );
+			strcpy( list[ listSize ].name, buffer );//Se llena el campo name
 			printf("Ingrese el email de %s:\n", buffer);
-			scanf("%99s", buffer);
-			strcpy(list[listSize].email, buffer);
+			scanf("%99s", buffer);//El buffer ha sido reutilizado
+			strcpy(list[listSize].email, buffer);//Se llena el campo email 
+			                                     //list[ListSize] es un arreglo
 			goOn = 1;
-			listSize++;
+			listSize++;//Este nos servira como indice en el arreglo al ingresar datos
+			           //y como factor para el reajuste de memoria  
 		} else {
 			goOn = 0;
 		}
